46e5e004c197fd9fb3ea75965662030c2ad43706
Utkarsh2311 <utkarsh.anand2311@gmail.com>
2021-05-14 08:44:24 -0400

    Uploading files

---
+++ dict.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+
+data=json.load(open('/data/data.json'))
+
+def dict(word):
+	word=word.lower()
+	if word in data:
+		return data[word]
+	elif len(get_close_matches(word,data.keys(),cutoff=0.8))>0:
+		
+		yn=input(f"Did you mean {get_close_matches(word,data.keys())[0]} instead,Enter Y if yes or N if no:")
+		if yn=='Y'or yn=='y':
+			return data[get_close_matches(word,data.keys())[0]]
+		elif yn!='Y' or yn!='y':
+			return 'we didnt get your query ,please enter again'
+		    
+
+		else:return 'Word is not in the Dictionary'
+	else:
+		return 'Word is not in the Dictionary,please enter another word'
+
+def out():
+	word=input('Enter the word: ')
+	output=dict(word)
+	if type(output)==list:
+		for i in output:
+			print(i)
+	else: print(output)
+
+out()
